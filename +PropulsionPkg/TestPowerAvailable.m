0830fb1d8c64bc0fed81c9a3e8d10396bda5028e
prmoko <prmoko@umich.edu>
2024-05-30 10:13:26 -0400

    Updated TestPowerAvailable
    Added a test with a turboelectric propulsion architecture.

--- +PropulsionPkg/TestPowerAvailable.m
+++ +PropulsionPkg/TestPowerAvailable.m
@@ -2,7 +2,7 @@ function [Success] = TestPowerAvailable()
 %
 % [Success] = TestPowerAvailable()
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 23 may 2024
+% last updated: 30 may 2024
 %
 % Generate simple test cases to confirm that the power available function
 % is working properly.
@@ -28,7 +28,7 @@ function [Success] = TestPowerAvailable()
 EPS06 = 1.0e-06;
 
 % assume all tests passed
-Pass = ones(5, 1);
+Pass = ones(6, 1);
 
 % ----------------------------------------------------------
 
@@ -349,6 +349,65 @@ TrueValue = [100; 100];
 
 % run the test
 Pass(5) = CheckTest(TestValue, TrueValue, EPS06);
+
+
+%% CASE 6: TURBOELECTRIC WITH NON-PERFECT EFFICIENCIES %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% setup the inputs           %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% set the SLS power and thrust (thrust can be arbitrary for prop aircraft)
+TestIn.Specs.Propulsion.SLSPower  = [160, 100];
+TestIn.Specs.Propulsion.SLSThrust = [  0,   0];
+
+% create the propulsion architecture
+TestIn.Specs.Propulsion.PropArch.TSPS = [0, 1];
+TestIn.Specs.Propulsion.PropArch.PSPS = [1, 0; 1, 1];
+TestIn.Specs.Propulsion.PropArch.PSES = [1; 0];
+
+% create the operational matrices
+TestIn.Specs.Propulsion.Oper.TS   = @() 1;
+TestIn.Specs.Propulsion.Oper.TSPS = @() [0, 1];
+TestIn.Specs.Propulsion.Oper.PSPS = @() [1, 0; 1, 1];
+TestIn.Specs.Propulsion.Oper.PSES = @() [1; 0];
+
+% create the efficiency matrices
+TestIn.Specs.Propulsion.Eta.TSPS = [1.0, 1.0];
+TestIn.Specs.Propulsion.Eta.PSPS = [1.0, 1.0; 0.625, 1.0];
+TestIn.Specs.Propulsion.Eta.PSES = [0.4; 1.0];
+
+% power split mission history
+TestIn.Mission.History.SI.Power.LamTS   = ones(2, 1);
+TestIn.Mission.History.SI.Power.LamTSPS = ones(2, 1);
+TestIn.Mission.History.SI.Power.LamPSPS = ones(2, 1);
+TestIn.Mission.History.SI.Power.LamPSES = ones(2, 1);
+
+% power source types
+TestIn.Specs.Propulsion.PropArch.PSType = [1, 0];
+
+% ----------------------------------------------------------
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% run the test               %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% compute the power available
+TestOut = PropulsionPkg.PowerAvailable(TestIn);
+
+% get the output to be tested
+TestValue = TestOut.Mission.History.SI.Power.TV;
+
+% list the correct values of the output
+TrueValue = [100; 100];
+
+% run the test
+Pass(6) = CheckTest(TestValue, TrueValue, EPS06);
 
 
 %% CHECK THE TEST RESULTS %%

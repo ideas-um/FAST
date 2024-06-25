a9c53693b1e24e581758aa92669e733861ae46ab
prmoko <prmoko@umich.edu>
2024-06-25 10:28:38 -0400

    Updated A320
    Reduced the number of points in the mission segment evaluation and added code to visualize a transport-like aircraft.

--- +AircraftSpecsPkg/A320Neo.m
+++ +AircraftSpecsPkg/A320Neo.m
@@ -2,19 +2,23 @@ function [Aircraft] = A320Neo()
 %
 % [Aircraft] = A320Neo()
 % written by Max Arnson, marnson@umich.edu
-% last updated: 14 feb 2024
+% modified by Paul Mokotoff, prmoko@umich.edu
+% last updated: 25 jun 2024
 % 
-% create a baseline model of the ERJ 175, long-range version (also known as
-% an ERJ 170-200). this version uses a conventional propulsion
+% Create a model of an Airbus A320neo with a conventional propulsion
 % architecture.
 %
-% all required inputs contain "** required **" before the description of
-% the parameter to be specified. all other parameters may remain as NaN,
-% and they will be filled in by a statistical regression. for improved
+% All required inputs contain "** required **" before the description of
+% the parameter to be specified. All other parameters may remain as NaN,
+% and they will be filled in by a statistical regression. For improved
 % accuracy, it is suggested to provide as many parameters as possible.
 %
-% inputs : none     
-% outputs: Aircraft - aircraft data structure to be used for analysis.
+% INPUTS:
+%     none
+%
+% OUTPUTS:
+%     Aircraft - aircraft data structure to be used for analysis
+%                size/type/units: 1-by-1 / struct / []
 %
 
 
@@ -26,42 +30,37 @@ Aircraft.Specs.TLAR.EIS = 2016;
 
 % ** required **
 % aircraft class, can be either:
-%     'Piston'    = piston engine
-%     'Turboprop' = turboprop engine
-%     'Turbofan'  = turbojet or turbofan engine
-Aircraft.Specs.TLAR.Class = 'Turbofan';
+%     "Piston"    = piston engine
+%     "Turboprop" = turboprop engine
+%     "Turbofan"  = turbojet or turbofan engine
+Aircraft.Specs.TLAR.Class = "Turbofan";
 
 % ** required **
-% approximate number of passengers
-Aircraft.Specs.TLAR.MaxPax = 15309/95;
+% approximate number of passengers (payload / average passenger mass)
+Aircraft.Specs.TLAR.MaxPax = 15309 / 95;
  
 
 %% VEHICLE PERFORMANCE %%
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
-% takeoff speed (kts)
-Aircraft.Specs.Performance.Vels.Tko = UnitConversionPkg.ConvVel(135,'kts','m/s');
+% takeoff speed (m/s)
+Aircraft.Specs.Performance.Vels.Tko = UnitConversionPkg.ConvVel(135, "kts", "m/s");
 
-% cruise speed (kts)
+% cruise speed (mach)
 Aircraft.Specs.Performance.Vels.Crs = 0.82;
 
-% specified speed type, either:
-%     'EAS' = equivalent airspeed
-%     'TAS' = true       airspeed
-Aircraft.Specs.Performance.Vels.Type = 'TAS';
+% takeoff altitude (m)
+Aircraft.Specs.Performance.Alts.Tko = 0;
 
-% takeoff altitude (ft)
-Aircraft.Specs.Performance.Alts.Tko =     0;
-
-% cruise altitude (ft)
-Aircraft.Specs.Performance.Alts.Crs = UnitConversionPkg.ConvLength(35000,'ft','m');
+% cruise altitude (m)
+Aircraft.Specs.Performance.Alts.Crs = UnitConversionPkg.ConvLength(35000, "ft", "m");
 
 % ** required **
-% design range (nmi)
+% design range (m)
 Aircraft.Specs.Performance.Range = 4815e3;
 
-% maximum rate of climb (ft/s), assumed 2,250 ft/min
-Aircraft.Specs.Performance.RCMax = UnitConversionPkg.ConvLength(2250/60,'ft','m');
+% maximum rate of climb (m/s), assumed 2,250 ft/min
+Aircraft.Specs.Performance.RCMax = UnitConversionPkg.ConvLength(2250/60, "ft", "m");
 
 
 %% AERODYNAMICS %%
@@ -71,35 +70,35 @@ Aircraft.Specs.Performance.RCMax = UnitConversionPkg.ConvLength(2250/60,'ft','m'
 crLDcf = 1.00; % aim for +/- 10%
 cbLDcf = 1.00; % aim for +/- 10%
 
-% lift-drag ratio during climb  (assumed same as ERJ175, standard range)
+% lift-drag ratio during climb
 Aircraft.Specs.Aero.L_D.Clb = 16 * cbLDcf;
 
-% lift-drag ratio during cruise (assumed same as ERJ175, standard range)
-Aircraft.Specs.Aero.L_D.Crs = NaN; %18.23 * crLDcf;
+% lift-drag ratio during cruise
+Aircraft.Specs.Aero.L_D.Crs = NaN; % was 18.23 * crLDcf
 
 % assume same lift-drag ratio during climb and descent
 Aircraft.Specs.Aero.L_D.Des = Aircraft.Specs.Aero.L_D.Clb;
 
-% wing loading (lbf / ft^2)
-Aircraft.Specs.Aero.W_S.SLS = 79000/126.5;
+% wing loading (kg / m^2)
+Aircraft.Specs.Aero.W_S.SLS = 79000 / 126.5;
 
 
 %% WEIGHTS %%
 %%%%%%%%%%%%%
 
-% maximum takeoff weight (lbm)
+% maximum takeoff weight (kg)
 Aircraft.Specs.Weight.MTOW = 79000;
 
-% electric generator weight (lbm)
+% electric generator weight (kg)
 Aircraft.Specs.Weight.EG = NaN;
 
-% electric motor weight (lbm)
+% electric motor weight (kg)
 Aircraft.Specs.Weight.EM = NaN;
 
-% block fuel weight (lbm)
+% block fuel weight (kg)
 Aircraft.Specs.Weight.Fuel = 19000;
 
-% battery weight (lbm), leave NaN for propulsion systems without batteries
+% battery weight (kg), leave NaN for propulsion systems without batteries
 Aircraft.Specs.Weight.Batt = NaN;
 
 
@@ -108,24 +107,25 @@ Aircraft.Specs.Weight.Batt = NaN;
 
 % ** required **
 % propulsion architecture, can be either:
-% 'AC'  = conventional
-% 'E'   = fully electric
-% 'PHE' = parallel hybrid electric
-% 'SHE' = series hybrid electric
-% 'TE'  = fully turboelectric
-% 'PE'  = partially turboelectric
+% "C"  = conventional
+% "E"   = fully electric
+% "PHE" = parallel hybrid electric
+% "SHE" = series hybrid electric
+% "TE"  = fully turboelectric
+% "PE"  = partially turboelectric
 Aircraft.Specs.Propulsion.Arch.Type = "C";
 
-% get the engine
+% **required** for configurations using gas-turbine engines
+% get the engine model
 Aircraft.Specs.Propulsion.Engine = EngineModelPkg.EngineSpecsPkg.LEAP_1A26;
 
 % number of engines
 Aircraft.Specs.Propulsion.NumEngines = 2;
 
 % thrust-weight ratio (if a turbojet/turbofan)
-Aircraft.Specs.Propulsion.T_W.SLS = 2.37e5/(73500*9.81);
+Aircraft.Specs.Propulsion.T_W.SLS = 2.37e5 / (73500 * 9.81);
 
-% total sea-level static thrust available (lbf)
+% total sea-level static thrust available (N)
 Aircraft.Specs.Propulsion.Thrust.SLS = 2.37e5;
 
 % engine propulsive efficiency
@@ -197,10 +197,10 @@ Aircraft.Specs.Power.Battery.BegSOC = NaN;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % number of control points in each segment
-Aircraft.Settings.TkoPoints = NaN;
-Aircraft.Settings.ClbPoints = NaN;
-Aircraft.Settings.CrsPoints = NaN;
-Aircraft.Settings.DesPoints = NaN;
+Aircraft.Settings.TkoPoints = 4;
+Aircraft.Settings.ClbPoints = 5;
+Aircraft.Settings.CrsPoints = 5;
+Aircraft.Settings.DesPoints = 5;
 
 % maximum number of iterations during oew estimation
 Aircraft.Settings.OEW.MaxIter = 50;
@@ -219,11 +219,24 @@ Aircraft.Settings.Analysis.Type = +1;
 % plotting, either:
 %     1 for plotting on
 %     0 for plotting off
-Aircraft.Settings.Plotting = 1;
+Aircraft.Settings.Plotting = 0;
 
+% return the mission history as a table (1) or not (0)
 Aircraft.Settings.Table = 0;
 
-Aircraft.Settings.VisualizeAircraft = 0;
+% flag to visualize the aircraft while sizing
+Aircraft.Settings.VisualizeAircraft = 1;
+
+% check if the aircraft should be visualized
+if (Aircraft.Settings.VisualizeAircraft == 1)
+    
+    % connect a geometry to the aircraft
+    Aircraft.Geometry.Preset = VisualizationPkg.GeometrySpecsPkg.Transport;    
+    
+    % specify a fuselage length
+    Aircraft.Geometry.LengthSet = convlength(180, "ft", "m");
+    
+end
 
 % ----------------------------------------------------------
 

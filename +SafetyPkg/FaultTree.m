028cb55387b4241b8a629e84ba8edc89d14c1184
prmoko <prmoko@umich.edu>
2024-08-02 10:44:35 -0400

    Logic Gates in FaultTree
    Added code to insert logic gates in the fault tree. Right now, any fault that is connected to one other component is assigned an 'OR' gate and any fault that is connected to multiple components is assigned an 'AND' gate.

--- +SafetyPkg/FaultTree.m
+++ +SafetyPkg/FaultTree.m
@@ -2,7 +2,7 @@ function [] = FaultTree()
 %
 %[] = FaultTree()
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 01 aug 2024
+% last updated: 02 aug 2024
 %
 % Create a fault tree from the adjacency matrix of a directed graph.
 %
@@ -12,6 +12,9 @@ function [] = FaultTree()
 % OUTPUTS:
 %     none
 %
+
+% cleanup before running
+close all
 
 %% TEST CASE %%
 %%%%%%%%%%%%%%%
@@ -39,7 +42,7 @@ A = [0, 1, 0, 0, 0, 0, 0, 0; ...
      0, 0, 0, 0, 0, 0, 0, 1; ...
      0, 0, 0, 0, 0, 0, 0, 0] ;
  
-% slightly more complex case 
+% slightly more complex case, multiple energy sources
 % A = [0, 0, 1, 0, 0; ...
 %      0, 0, 1, 0, 0; ...
 %      0, 0, 0, 1, 0; ...
@@ -68,59 +71,145 @@ NodeLabels = [strcat("Element " + num2str((1:nnode-1)') + " Fail"); ...
               "Loss of Thrust"];
 
 % create the graph
-G = digraph(A, NodeLabels);
+G0 = digraph(A, NodeLabels);
 
-% plot the tree
-Tree = plot(G);
+% % plot the tree
+% figure;
+% T0 = plot(G0);
+% 
+% % turn off edges
+% T0.ArrowSize = 0;
 
-% turn off edges
-Tree.ArrowSize = 0;
+
+%% ADD THE LOGIC GATES %%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% get all possible paths
+BPath = bfsearch(G0, isrc, "allevents");
+
+% get the number of edges
+nedge = height(G0.Edges);
+
+% make a larger matrix to include the logical gates
+B = [A, zeros(nnode, nedge); zeros(nedge, nnode), zeros(nedge)];
+
+% remember the column index for new logic gates
+idx = nnode + 1;
+
+% loop through the paths searched
+for istep = 1:height(BPath)
+    
+    % get the event
+    CurEvent = BPath.Event(istep);
+    
+    if      (CurEvent == "finishnode"      )
+        
+        % increment the index
+        idx = idx + 1;
+        
+    elseif ((CurEvent == "edgetonew"       ) || ...
+            (CurEvent == "edgetodiscovered"))
+        
+        % remove the existing path
+        B(BPath.Edge(istep, 1), BPath.Edge(istep, 2)) = 0;
+        
+        % add in a stop at the logic gate
+        B(BPath.Edge(istep, 1), idx                ) = 1;
+        B(idx                , BPath.Edge(istep, 2)) = 1;
+        
+    end        
+end
+
+% memory for the gates' names
+GateLabels = repmat("", nedge, 1);
+
+% check how many connections leave the nodes
+OutOrder = sum(B(nnode+1:end, :), 2);
+
+% get the number of OR and AND gates
+nor  = OutOrder == 1;
+nand = OutOrder >  1;
+
+% update the gate labels
+GateLabels(nor ) = strcat("OR" , num2str((1:sum(nor ))'));
+GateLabels(nand) = strcat("AND", num2str((1:sum(nand))'));
+
+% create new node labels
+NodeLabels = [NodeLabels; GateLabels];
+
+% find vertices that are islands
+Island = find((sum(B, 1)' == 0) & (sum(B, 2) == 0));
+
+% remove the islands' rows/columns from the adjacency matrix
+B(Island, :) = [];
+B(:, Island) = [];
+
+% remove the islands' node labels
+NodeLabels(Island) = [];
+
+% create a new digraph
+G1 = digraph(B, NodeLabels);
+
+% plot the new graph
+figure;
+T1 = plot(G1);
+
+% turn off the edges
+T1.ArrowSize = 0;
 
 
 %% RE-ARRANGE THE DATA TO LOOK LIKE A TREE %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% get the number of nodes in the new graph
+[nnode, ~] = size(B);
+
+% get the sources
+isrc = find(sum(B, 1) == 0);
+
 % find the maximum level that the fault can occur
 MaxLevel = zeros(nnode, 1);
 
-% loop through all possible paths
-for ipath = 1:length(isrc)
+% get all possible paths
+DPath = dfsearch(G1, isrc, "allevents");
+
+% get the first level
+ylevel = 1;
+
+% loop through the path elements
+for istep = 1:height(DPath)
     
-    % get the path
-    Path = dfsearch(G, isrc(ipath), "allevents");
+    % get the event
+    CurEvent = DPath.Event(istep);
     
-    % get the first level
-    ylevel = 0;
-    
-    % loop through the path elements
-    for istep = 1:height(Path)
+    % take the appropriate course of action during the search
+    if     (CurEvent == "discovernode")
         
-        % get the event
-        CurEvent = Path.Event(istep);
+        % if a vertex was discovered, add a layer
+        ylevel = ylevel + 1;
         
-        % if a node was discovered, add a layer
-        if (CurEvent == "discovernode")
-            ylevel = ylevel + 1;
-            
-        elseif (CurEvent == "finishnode")
-            ylevel = ylevel - 1;
-            
-        else
-            continue;
-            
-        end
+    elseif (CurEvent == "finishnode"  )
         
-        % get the node
-        inode = Path.Node(istep);
+        % if a vertex has no edges, retreat a layer
+        ylevel = ylevel - 1;
+                
+    else
         
-        % check which layer it should be placed on
-        MaxLevel(inode) = max(MaxLevel(inode), ylevel);
+        % no action needed
+        continue;
         
     end
+    
+    % get the node
+    inode = DPath.Node(istep);
+    
+    % check which layer it should be placed on
+    MaxLevel(inode) = max(MaxLevel(inode), ylevel);
+    
 end
 
 % find the total number of levels
-nlevel = max(MaxLevel) + 1;
+nlevel = max(MaxLevel);
 
 % re-arrange the elements in the tree
 for ilevel = 1:nlevel
@@ -129,10 +218,10 @@ for ilevel = 1:nlevel
     lnode = MaxLevel == ilevel;
     
     % spread out x-variables
-    Tree.XData(lnode) = 1:sum(lnode);
+    T1.XData(lnode) = 1:sum(lnode);
     
     % set the y-variable
-    Tree.YData(lnode) = nlevel - ilevel;
+    T1.YData(lnode) = nlevel - ilevel;
     
 end
 

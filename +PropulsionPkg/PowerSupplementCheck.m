6f8f2a350efa3573b8d5f563c83f5b15113fa964
prmoko <prmoko@umich.edu>
2024-09-27 09:07:50 -0400

    Updated PowerSupplementCheck
    Vectorized the code such that multiple timesteps can be assessed simultaneously. Also, added multiple test cases to confirm that the code works properly.

--- +PropulsionPkg/PowerSupplementCheck.m
+++ +PropulsionPkg/PowerSupplementCheck.m
@@ -1,13 +1,18 @@
-function [Psupp] = PowerSupplementCheck()
+function [Psupp] = PowerSupplementCheck(iprob)
 %
-% [Psupp] = PowerSupplementCheck()
+% [Psupp] = PowerSupplementCheck(iprob)
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 26 sep 2024
+% last updated: 27 sep 2024
 %
-% check new approach to accounting for power supplements.
+% In the propulsion architecture, check if any components are either
+% suppling/siphoning power from the gas-turbine engines. If a component is
+% placed in series after a gas-turbine engine, it siphons power (negative
+% output). If a component is placed in parallel with a gas-turbine engine,
+% it provides power (positive output, unless the flow is reversed).
 %
 % INPUTS:
-%     none
+%     iprob - the problem to be solved.
+%             size/type/units: 1-by-1 / integer / []
 %
 % OUTPUTS:
 %     Psupp - the supplemental power provided/required into each power
@@ -19,45 +24,90 @@ function [Psupp] = PowerSupplementCheck()
 %% ESTABLISH ARBITRARY PARAMETERS FOR NOW %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% series/parallel
-% PSPS = [1, 0; 1, 1];
-% PreqDr = [80, 20];
-% ipnt = 1;
-% SplitPSPS = [1, 0; 0.9, 1];
-% EtaPSPS = [1, 1; 1, 1];%0.96, 1];
-% TSPS = [1, 1];
-% PSType = [1, 0];
-% FanEfficiency = 1;%0.99;
+if     (iprob == 1)
+    
+    % series/parallel
+    PSPS = [1, 0; 1, 1];
+    PreqDr = [80, 20; 70, 30];
+    SplitPSPS = [1, 0; 0.9, 1];
+    EtaPSPS = [1, 1; 1, 1];
+    TSPS = [1, 1];
+    PSType = [1, 0];
+    FanEfficiency = 1;
+    
+elseif (iprob == 2)
 
-% series only
-% PSPS = [1, 0; 1, 1];
-% PreqDr = [10, 90];
-% ipnt = 1;
-% SplitPSPS = [1, 0; 1, 1];
-% EtaPSPS = [1, 1; 1, 1];
-% TSPS = [1, 0; 0, 1];
-% PSType = [1, 0];
-% FanEfficiency = 1;
+    % series/parallel, but swap order
+    PSPS = [1, 1; 0, 1];
+    PreqDr = [20, 80; 30, 70];
+    SplitPSPS = [1, 0.9; 0, 1];
+    EtaPSPS = [1, 1; 1, 1];
+    TSPS = [1, 1];
+    PSType = [0, 1];
+    FanEfficiency = 1;
+    
+elseif (iprob == 3)
 
-% parallel only
-PSPS = [1, 0; 0, 1];
-PreqDr = [80, 20];
-ipnt = 1;
-SplitPSPS = [1, 0; 0, 1];
-EtaPSPS = [1, 1; 1, 1];
-TSPS = [1, 1];
-PSType = [1, 0];
-FanEfficiency = 1;
+    % series only
+    PSPS = [1, 0; 1, 1];
+    PreqDr = [10, 90; 15, 85];
+    SplitPSPS = [1, 0; 1, 1];
+    EtaPSPS = [1, 1; 1, 1];
+    TSPS = [1, 0; 0, 1];
+    PSType = [1, 0];
+    FanEfficiency = 1;
+
+elseif (iprob == 4)
+    
+    % series only, but swap order
+    PSPS = [1, 1; 0, 1];
+    PreqDr = [90, 10; 85, 15];
+    SplitPSPS = [1, 1; 0, 1];
+    EtaPSPS = [1, 1; 1, 1];
+    TSPS = [0, 1; 1, 0];
+    PSType = [0, 1];
+    FanEfficiency = 1;
+
+elseif (iprob == 5)
+    
+    % parallel only
+    PSPS = [1, 0; 0, 1];
+    PreqDr = [80, 20; 90, 10];
+    SplitPSPS = [1, 0; 0, 1];
+    EtaPSPS = [1, 1; 1, 1];
+    TSPS = [1, 1];
+    PSType = [1, 0];
+    FanEfficiency = 1;
+
+elseif (iprob == 6)
+    
+    % parallel only, but swap order
+    PSPS = [1, 0; 0, 1];
+    PreqDr = [20, 80; 10, 90];
+    SplitPSPS = [1, 0; 0, 1];
+    EtaPSPS = [1, 1; 1, 1];
+    TSPS = [1, 1];
+    PSType = [0, 1];
+    FanEfficiency = 1;
+    
+else
+    
+    % don't solve
+    Psupp = 0;
+    
+    return
+    
+end    
 
 
 %% PERFORM THE ANALYSIS %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% get the number of power sources
-[nps, ~] = size(PSPS);
+% get the number of points
+[npnt, nps] = size(PreqDr);
 
 % allocate memory for the supplemental power
-Psupp = zeros(1, nps);
+Psupp = zeros(npnt, nps);
 
 % check for components in series
 AnySeries = find(sum(PSPS - eye(nps), 1) > 0);
@@ -81,7 +131,7 @@ if (any(AnySeries))
         MyConns = find(PSPS(:, icomp) - RemoveDriving > 0);
         
         % account for the power siphon
-        Psupp(icomp) = Psupp(icomp) - PreqDr(ipnt, MyConns) * ...
+        Psupp(:, icomp) = Psupp(:, icomp) - PreqDr(:, MyConns) * ...
                        (SplitPSPS(MyConns, icomp) ./ EtaPSPS(MyConns, icomp));
                    
         % reset the driving source
@@ -117,7 +167,7 @@ if (any(AnyParallel))
         Helping = find((TSPS(icomp, :) > 0) & (PSType == 0));
                 
         % add the power supplement
-        Psupp(icomp) = Psupp(icomp) + PreqDr(ipnt, Helping) ./ FanEfficiency; %#ok<FNDSB>, ignore warning about "find" ... easier to read this way
+        Psupp(:, Driving) = Psupp(:, Driving) + PreqDr(:, Helping) ./ FanEfficiency; %#ok<FNDSB>, ignore warning about "find" ... easier to read this way
         
     end % for
 end % if

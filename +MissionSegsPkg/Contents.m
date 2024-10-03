5404825fb6440d85171c2c5d20bb114d1782318d
prmoko <prmoko@umich.edu>
2024-10-03 09:35:47 -0400

    Updated Contents Files
    Updated the Contents.m files to match the files rpesent in each directory.

--- +MissionSegsPkg/Contents.m
+++ +MissionSegsPkg/Contents.m
@@ -9,6 +9,7 @@
 % EvalCruise                    - MissionSegsPkg.EvalCruise is a function.
 % EvalCruiseBRE                 - MissionSegsPkg.EvalCruiseBRE is a function.
 % EvalDescent                   - MissionSegsPkg.EvalDescent is a function.
+% EvalDetailedTakeoff           - MissionSegsPkg.EvalDetailedTakeoff is a function.
 % EvalLanding                   - MissionSegsPkg.EvalLanding is a function.
 % EvalTakeoff                   - MissionSegsPkg.EvalTakeoff is a function.
 % FlyMission                    - MissionSegsPkg.FlyMission is a function.

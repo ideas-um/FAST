ccff63c46f859b6c753ce810fa9a9565646de67d
prmoko <prmoko@umich.edu>
2024-10-04 10:20:01 -0400

    Updated FaultTree
    Corrected the node-gate connections that were previously erroneous. Instead of considering the number of edges in the graph and the order that the nodes are traversed, just allocate enough memory for all of the nodes (which include failures and logic gates).

--- +SafetyPkg/FaultTree.m
+++ +SafetyPkg/FaultTree.m
@@ -2,7 +2,7 @@ function [] = FaultTree(Arch, Components)
 %
 % [] = FaultTree(Arch, Components)
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 30 sep 2024
+% last updated: 04 oct 2024
 %
 % Create a fault tree from the adjacency matrix of a directed graph with
 % the names of the given components.
@@ -33,7 +33,7 @@ isrc = find(sum(Arch, 1) == 0);
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 % generate node labels (sink shows thrust generated, so label separately)
-NodeLabels = strcat("Loss of ", Components.Name);
+NodeLabels = Components.Name;
 
 % create the graph
 G0 = digraph(Arch, NodeLabels);
@@ -45,17 +45,8 @@ G0 = digraph(Arch, NodeLabels);
 % get all possible paths
 BPath = bfsearch(G0, isrc, "allevents");
 
-% get the number of edges
-nedge = height(G0.Edges);
-
 % make a larger matrix to include the logical gates
-B = [Arch, zeros(nnode, nedge); zeros(nedge, nnode), zeros(nedge)];
-
-% allocate memory to remember the nodes corresponding to each edge
-ColIdx = zeros(nnode, 1);
-
-% remember the column index for new logic gates
-idx = nnode;
+B = [Arch, zeros(nnode, nnode); zeros(nnode, 2*nnode)];
 
 % loop through the paths searched
 for istep = 1:height(BPath)
@@ -63,23 +54,15 @@ for istep = 1:height(BPath)
     % get the event
     CurEvent = BPath.Event(istep);
     
-    if      (CurEvent == "discovernode"    )
-        
-        % increment the index
-        idx = idx + 1;
-        
-        % remember the index
-        ColIdx(BPath.Node(istep)) = idx;
-        
-    elseif ((CurEvent == "edgetonew"       ) || ...
-            (CurEvent == "edgetodiscovered"))
+    if ((CurEvent == "edgetonew"       ) || ...
+        (CurEvent == "edgetodiscovered"))
         
         % get the beginning and ending nodes
         BegNode = BPath.Edge(istep, 1);
         EndNode = BPath.Edge(istep, 2);
         
-        % get the index of the beginning node
-        icol = ColIdx(BegNode);
+        % offset index by number of components in the system
+        icol = BegNode + nnode;
         
         % remove the existing path
         B(BegNode, EndNode) = 0;
@@ -91,11 +74,8 @@ for istep = 1:height(BPath)
     end        
 end
 
-% offset the gate column indexes to access the nodes
-ColIdx = ColIdx - nnode;
-
 % remember for the gates' names
-GateLabels = strcat(Components.GateType(ColIdx), num2str(Components.GateIdx(ColIdx)));
+GateLabels = strcat(Components.GateType, num2str(Components.GateIdx));
 
 % create new node labels
 NodeLabels = [NodeLabels; GateLabels];
@@ -111,82 +91,23 @@ B(:, Island) = [];
 NodeLabels(Island) = [];
 
 
-%% RE-ARRANGE THE DATA TO LOOK LIKE A TREE %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% RE-MAKE THE FAULT TREE %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % create a new digraph
 G1 = digraph(B, NodeLabels);
 
-% plot the new graph
+% create a figure
 figure;
+
+% plot the new graph
 T1 = plot(G1);
 
-% turn off the edges
+% format the plot
 T1.ArrowSize = 0;
-
-% get the number of nodes in the new graph
-[nnode, ~] = size(B);
-
-% get the sources
-isrc = find(sum(B, 1) == 0);
-
-% find the maximum level that the fault can occur
-MaxLevel = zeros(nnode, 1);
-
-% get all possible paths
-DPath = dfsearch(G1, isrc, "allevents");
-
-% get the first level
-ylevel = 1;
-
-% loop through the path elements
-for istep = 1:height(DPath)
-    
-    % get the event
-    CurEvent = DPath.Event(istep);
-    
-    % take the appropriate course of action during the search
-    if     (CurEvent == "discovernode")
-        
-        % if a vertex was discovered, add a layer
-        ylevel = ylevel + 1;
-        
-    elseif (CurEvent == "finishnode"  )
-        
-        % if a vertex has no edges, retreat a layer
-        ylevel = ylevel - 1;
-                
-    else
-        
-        % no action needed
-        continue;
-        
-    end
-    
-    % get the node
-    inode = DPath.Node(istep);
-    
-    % check which layer it should be placed on
-    MaxLevel(inode) = max(MaxLevel(inode), ylevel);
-    
-end
-
-% find the total number of levels
-nlevel = max(MaxLevel);
-
-% re-arrange the elements in the tree
-for ilevel = 1:nlevel
-    
-    % remember the nodes on this level
-    lnode = MaxLevel == ilevel;
-    
-    % spread out x-variables
-    T1.XData(lnode) = 1:sum(lnode);
-    
-    % set the y-variable
-    T1.YData(lnode) = nlevel - ilevel;
-    
-end
+T1.NodeFontSize = 12;
+T1.MarkerSize = 6;
+T1.LineWidth = 2;
 
 % ----------------------------------------------------------
 

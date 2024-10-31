86586bfca1848e45c427930bae914040eac99fe0
prmoko <prmoko@umich.edu>
2024-10-31 08:25:56 -0400

    Added TutorialsPkg Documentation
    Added a README and a Contents file to the TutorialsPkg.

---
+++ +TutorialsPkg/Contents.m
@@ -0,0 +1,21 @@
+% Contents of TutorialsPkg package:
+%
+% README          - TutorialsPkg.README is a function.
+%
+% Functions
+% -------------------------------------------------------------------------
+% Tutorial002     - TutorialsPkg.Tutorial002 is a function.
+% Tutorial003     - TutorialsPkg.Tutorial003 is a function.
+% Tutorial004     - TutorialsPkg.Tutorial004 is a function.
+% Tutorial005     - TutorialsPkg.Tutorial005 is a function.
+% Tutorial007     - TutorialsPkg.Tutorial007 is a function.
+% Tutorial008     - TutorialsPkg.Tutorial008 is a function.
+% Tutorial009     - TutorialsPkg.Tutorial009 is a function.
+% Tutorial010     - TutorialsPkg.Tutorial010 is a function.
+% Tutorial011     - TutorialsPkg.Tutorial011 is a function.
+% Tutorial012.m   - TutorialsPkg.Tutorial012.m is a function.
+%
+% MAT-Files
+% -------------------------------------------------------------------------
+% Tutorial012.mat - TutorialsPkg.Tutorial012.mat is a MAT-File.
+%

c65d0c7ad9a7fe4dbb81cc9c13a994be17c5598b
prmoko <prmoko@umich.edu>
2024-11-19 15:28:30 -0500

    Updated AssessFaultTree
    Improved the function such that it creates a string of failures based on the system architecture provided. Then, Matlab's symbolics are used to simplify the Boolean expression before substituting in the failure probabilities and computing the system's probability of failure.

--- +SafetyPkg/AssessFaultTree.m
+++ +SafetyPkg/AssessFaultTree.m
@@ -1,8 +1,8 @@
-function [] = AssessFaultTree(Arch, Components, RemoveSrc)
+function [Pfail] = AssessFaultTree(Arch, Components, RemoveSrc)
 %
 % AssessFaultTree.m
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 06 nov 2024
+% last updated: 19 nov 2024
 %
 % Given an adjacency-like matrix, assemble a fault tree that accounts for
 % internal failures and redundant primary events.
@@ -10,7 +10,7 @@ function [] = AssessFaultTree(Arch, Components, RemoveSrc)
 % INPUTS:
 %     Arch       - the architecture matrix representing the system
 %                  architecture to be analyzed.
-%                  size/type/units: n-by-n / int / []
+%                  size/type/units: n-by-n / integer / []
 %
 %     Components - a structure array containing each component in the
 %                  system architecture and the following information about
@@ -23,11 +23,14 @@ function [] = AssessFaultTree(Arch, Components, RemoveSrc)
 %                  size/type/units: 1-by-1 / struct / []
 %
 %     RemoveSrc  - a flag to indicate whether the sources should be removed
-%                  from the system architecture (1) or not (0).
-%                  size/type/units: 1-by-1 / int / []
+%                  from the system architecture (1) or not (0). if no
+%                  argument is provided, the default is to not remove any
+%                  components (0).
+%                  size/type/units: 1-by-1 / integer / []
 %
 % OUTPUTS:
-%     none
+%     Pfail      - the probability that the system fails.
+%                  size/type/units: 1-by-1 / double / []
 %
 
 
@@ -106,7 +109,7 @@ end
 
 %% PRE-PROCESSING %%
 %%%%%%%%%%%%%%%%%%%%
-
+    
 % count the number of input/output connections
 ninput  = sum(Arch, 1)';
 noutput = sum(Arch, 2) ;
@@ -128,46 +131,99 @@ end
 
 % check if sources must be removed
 if (RemoveSrc == 1)
-        
+    
     % remove their connections, but keep them in the matrix
     Arch(isrc, :) = 0; %#ok<*FNDSB>
     
-    % re-count the number of input/output connections
-    ninput = sum(Arch, 1)';
-    
 end
-
-% get the number of sources
-nsrc = length(isrc);
 
 
 %% PERFORM A BOOLEAN ANALYSIS %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% assemble the string
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% get all failure modes from %
+% the system architecture    %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% initialize a string to list all possible failure modes
 Failure = "";
 
-% recursively search the graph
+% recursively search the system architecture to extract all failure modes
 Failure = CreateFailureExp(Arch, Components, isnk, Failure);
 
-split(Failure, ["(", ")", "+"])
+% convert the failure modes into a symbolic expression
+MyFT = str2sym(Failure);
+
+% expand the fault tree first and then simplify
+SimpFT = simplify(expand(MyFT), "Steps", 10 * ncomp);
+
+% convert the simplified fault tree to a string
+FailString = string(SimpFT);
+
+% print the failure modes after simplifying
+fprintf(1, "Final String: %s\n", FailString);
+
+
+%% COMPUTE PROBABILITY OF FAILURE %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% substitute the failure rates for their respective failure mode
+WithNums = replace(FailString, [Components.Name; "&"; "|"], [string(Components.FailRate); "*"; "+"]);
+
+% evaluate the string to find the probability of failure
+Pfail = double(eval(WithNums));
 
 % ----------------------------------------------------------
 
 end
 
+% ----------------------------------------------------------
+% ----------------------------------------------------------
+% ----------------------------------------------------------
+
 function [Failure] = CreateFailureExp(Arch, Components, icomp, Failure)
 %
 % [Failure] = CreateFailureExp(Arch, Components, icomp, Failure)
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 06 nov 2024
+% last updated: 18 nov 2024
 %
-% description.
+% Create a symbolic failure expression by recursively searching the fault
+% tree. For each function call, check whether an internal failure mode
+% exists and if there are any downstream components that need to be
+% considered in this failure mode.
 %
 % INPUTS:
+%     Arch       - the architecture matrix representing the system
+%                  architecture to be analyzed.
+%                  size/type/units: n-by-n / int / []
+%
+%     Components - a structure array containing each component in the
+%                  system architecture and the following information about
+%                  it:
+%                      a) the component name, a string
+%                      b) a column vector of failure rates
+%                  size/type/units: 1-by-1 / struct / []
+%
+%     icomp      - the index of the component in the fault tree currently
+%                  being assessed.
+%                  size/type/units: 1-by-1 / integer / []
+%
+%     Failure    - a string to be updated with additional failure modes.
+%                  size/type/units: 1-by-1 / string / []
 %
 % OUTPUTS:
+%     Failure    - the input string updated with all of the necessary
+%                  failure modes after recursively searching the fault
+%                  tree.
+%                  size/type/units: 1-by-1 / string / []
 %
+
+
+%% CHECK FOR AN INTERNAL FAILURE %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % get the downstream components
 idwn = find(Arch(:, icomp));
@@ -181,12 +237,15 @@ if (~strcmpi(Components.FailMode(icomp), "") == 1)
     % check if there are downstream components
     if (~isempty(idwn) == 1)
         
-        % add a plus
-        Failure = Failure + "+";
+        % add an OR gate
+        Failure = Failure + "|";
         
     end
 end
 
+
+%% CHECK FOR DOWNSTREAM FAILURES %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % get the number of downstream components
 ndwn = length(idwn);
@@ -199,7 +258,7 @@ for i = 1:ndwn
         Failure = Failure + "(";
     end
     
-    % search recursively
+    % search recursively and add on to the string
     Failure = CreateFailureExp(Arch, Components, idwn(i), Failure);
     
     % add parenthesis
@@ -207,9 +266,9 @@ for i = 1:ndwn
         Failure = Failure + ")";
     end
     
-    % multiply, if needed
+    % add an AND gate, if needed
     if (i < ndwn)
-        Failure = Failure + "*";
+        Failure = Failure + "&";
     end
 end
 

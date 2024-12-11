6e92f1d46ce9fec786568ac70237d6c14399bd6e
prmoko <prmoko@umich.edu>
2024-12-11 08:27:46 -0500

    Updated RecomputeSplits
    Overhauled the function for the new propulsion architecture definition.

--- +PropulsionPkg/RecomputeSplits.m
+++ +PropulsionPkg/RecomputeSplits.m
@@ -2,7 +2,7 @@ function [Aircraft] = RecomputeSplits(Aircraft, SegBeg, SegEnd)
 %
 % [Aircraft] = RecomputeSplits(Aircraft, SegBeg, SegEnd)
 % written by Paul Mokotoff, prmoko@umich.edu
-% last updated: 23 apr 2024
+% last updated: 11 dec 2024
 %
 % Re-compute the operational power splits for a "full throttle" setting
 % during the mission.
@@ -35,23 +35,29 @@ function [Aircraft] = RecomputeSplits(Aircraft, SegBeg, SegEnd)
 % get the parallel connections
 ParConns = Aircraft.Specs.Propulsion.PropArch.ParConns;
 
-% check for any parallel connections
+% identify any parallel connections
 ParIndx = find(cellfun(@(x) ~isempty(x), ParConns));
 
-% get the number of parallel connections
-npar = length(ParIndx);
-
-% get the power available (equal to power output for "full throttle" case)
-Pav_PS = Aircraft.Mission.History.SI.Power.Pav_PS(SegBeg:SegEnd, :);
+% check if there are any parallel connections
+if (~any(ParIndx))
+    
+    % don't re-compute if there aren't any splits
+    return
+    
+end
 
 
 %% RE-COMPUTE THE POWER SPLITS %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% don't re-compute if there aren't any splits
-if (npar < 1)
-    return
-end
+% get the number of parallel connections
+npar = length(ParIndx);
+
+% get the number of sources and transmitters
+nsrc = length(Aircraft.Specs.Propulsion.PropArch.SrcType);
+
+% get the power available (equal to power output for "full throttle" case)
+Pav = Aircraft.Mission.History.SI.Power.Pav(SegBeg:SegEnd, :);
 
 % loop through each power split
 for ipar = 1:npar
@@ -63,18 +69,15 @@ for ipar = 1:npar
     isupp = ParConns{imain};
     
     % get the total power output at any given time from those sources
-    Pout = sum(Pav_PS(:, [imain, isupp]), 2);
+    Pout = sum(Pav(:, [imain+nsrc, isupp]), 2);
     
     % compute the downstream power split
-    SplitTSPS = Pav_PS(:, [imain, isupp]) ./ Pout;
-    
-    % remember the actual power split
-    LamTSPS = SplitTSPS(:, end);
-    
+    LamDwn = Pav(:, isupp) ./ Pout;
+        
 end
 
 % remember the power split
-Aircraft.Mission.History.SI.Power.LamTSPS(SegBeg:SegEnd, :) = LamTSPS;
+Aircraft.Mission.History.SI.Power.LamDwn(SegBeg:SegEnd, :) = LamDwn(:, end);
 
 % ----------------------------------------------------------
 

10ea7a579505d20f30327c2604e87f64b6087fa7
emmasmit5 <80856344+emmasmit5@users.noreply.github.com>
2025-03-11 14:01:19 -0400

    updates

--- +ExperimentPkg/SeqAnalysis.m
+++ +ExperimentPkg/SeqAnalysis.m
@@ -30,62 +30,76 @@ totFuel=OptACs.OptAircraft1.Specs.Weight.Fuel + ...
         alt = [alt1; alt2; alt3; alt4;];
         t = [t1; t2; t3; t4]./60./60;
         
-        figure;
-        plot(t, alt, "k", "LineWidth", 1)
-        xlabel("Time (hr)")
-        ylabel("Alt (m)")
-        title("Sequence Flight Profile")
 
 
-
-OAC = OptimizedAircraft;
-NAC = NonOptAC;
-
-[of, ot] = seqanal(OAC);
-[nf, nt] = seqanal(NAC);
-
-dif = (ot - nt)./nt*100;
-
+opt = MakeTTable(OptimizedAircraft);
 figure;
-plot(nt, nf, "LineWidth",1.5)
-hold on
-plot(ot, of, "LineWidth",1.5)
-title("Cummulative Fuel Burn across Sequence")
-xlabel("Time (hr)")
-ylabel("Cummulative Fuel Burn (kg)")
+stackedplot(opt, ["Altitude (m)", "Fuel Burn (kg)", "Ebatt (kWh)", "GT PC", "EM PC"]);
+title("AC Performance");
 
-yyaxis right
-plot(ot, dif, "LineWidth",1)
-ylabel("Cummulative Fuel Burn % Difference")
-legend("Non-Optimized HEA", "Optimized HEA", "% Diff")
-set(gca, "FontSize", 12)
+og = MakeTTable(NonOptAC);
 
-function [f, t] = seqanal(OptACs)
+tthist = synchronize(opt, og, 'union', 'linear');
+figure;
+stackedplot(tthist, ["Altitude (m)_opt", "Fuel Burn (kg)_opt", "Fuel Burn (kg)_og", "SOC_opt", "SOC_og", "GT PC_opt", "GT PC_og", "EM PC_opt", "EM PC_og"]);
+
+
+
+function [History] = MakeTTable(OptACs)
 fburn1 = OptACs.OptAircraft1.Mission.History.SI.Weight.Fburn(1:73);
 fburn2 = OptACs.OptAircraft2.Mission.History.SI.Weight.Fburn(1:73) + fburn1(end);
 fburn3 = OptACs.OptAircraft3.Mission.History.SI.Weight.Fburn(1:73) + fburn2(end);
 fburn4 = OptACs.OptAircraft4.Mission.History.SI.Weight.Fburn(1:73) + fburn3(end);
-f = [fburn1; fburn2; fburn3; fburn4;];
+fburn = [fburn1; fburn2; fburn3; fburn4;];
 
+e1 = OptACs.OptAircraft1.Mission.History.SI.Energy.E_ES(1:73, 2);
+e2 = OptACs.OptAircraft2.Mission.History.SI.Energy.E_ES(1:73, 2) + e1(end);
+e3 = OptACs.OptAircraft3.Mission.History.SI.Energy.E_ES(1:73, 2) + e2(end);
+e4 = OptACs.OptAircraft4.Mission.History.SI.Energy.E_ES(1:73, 2) + e3(end);
+e = [e1; e2; e3; e4;]./3600./1000;%kWh
+
+GTPC1 = OptACs.OptAircraft1.Mission.History.SI.Power.PC(1:73, 1);
+GTPC1(end) = 0;
+GTPC2 = OptACs.OptAircraft2.Mission.History.SI.Power.PC(1:73, 1);
+GTPC2(end) = 0;
+GTPC3 = OptACs.OptAircraft3.Mission.History.SI.Power.PC(1:73, 1);
+GTPC3(end) = 0;
+GTPC4 = OptACs.OptAircraft4.Mission.History.SI.Power.PC(1:73, 1);
+GTPC4(end) = 0;
+GTPC = [GTPC1; GTPC2; GTPC3; GTPC4;];
+
+EMPC1 = OptACs.OptAircraft1.Mission.History.SI.Power.PC(1:73, 3);
+EMPC2 = OptACs.OptAircraft2.Mission.History.SI.Power.PC(1:73, 3);
+EMPC3 = OptACs.OptAircraft3.Mission.History.SI.Power.PC(1:73, 3);
+EMPC4 = OptACs.OptAircraft4.Mission.History.SI.Power.PC(1:73, 3);
+EMPC = [EMPC1; EMPC2; EMPC3; EMPC4;];
+SOC1 = OptACs.OptAircraft1.Mission.History.SI.Power.SOC(1:73, 2);
+SOC2 = OptACs.OptAircraft2.Mission.History.SI.Power.SOC(1:73, 2);
+SOC3 = OptACs.OptAircraft3.Mission.History.SI.Power.SOC(1:73, 2);
+SOC4 = OptACs.OptAircraft4.Mission.History.SI.Power.SOC(1:73, 2);
+SOC = [SOC1; SOC2; SOC3; SOC4;];
+
+alt1 = OptACs.OptAircraft1.Mission.History.SI.Performance.Alt(1:73);
+alt1(end) = 0;
 t1 = OptACs.OptAircraft1.Mission.History.SI.Performance.Time(1:73);
+
+alt2 = OptACs.OptAircraft2.Mission.History.SI.Performance.Alt(1:73);
+alt2(end) = 0;
 t2 = OptACs.OptAircraft2.Mission.History.SI.Performance.Time(1:73) + t1(end) + 46*60;
+
+alt3 = OptACs.OptAircraft3.Mission.History.SI.Performance.Alt(1:73);
 t3 = OptACs.OptAircraft3.Mission.History.SI.Performance.Time(1:73) + t2(end) + 32*60;
+alt3(end) = 0;
+alt4 = OptACs.OptAircraft4.Mission.History.SI.Performance.Alt(1:73);
 t4 = OptACs.OptAircraft4.Mission.History.SI.Performance.Time(1:73)+t3(end)+ 27*60;
-t = [t1; t2; t3; t4]./60./60;
+alt4(end) = 0;
+alt = [alt1; alt2; alt3; alt4;];
+t = [t1; t2; t3; t4]./60;
 
-end
+% make miss hist table
+time = minutes(t);
+mytable = table(time, alt, fburn, e, SOC, GTPC, EMPC);
+mytable.Properties.VariableNames = string(["Time (min)", "Altitude (m)", "Fuel Burn (kg)", "Ebatt (kWh)", "SOC", "GT PC", "EM PC"]);
 
-function [f, t] = seqanal(OptACs)
-fburn1 = OptACs.OptAircraft1.Mission.History.SI.Weight.Fburn(1:73);
-fburn2 = OptACs.OptAircraft2.Mission.History.SI.Weight.Fburn(1:73) + fburn1(end);
-fburn3 = OptACs.OptAircraft3.Mission.History.SI.Weight.Fburn(1:73) + fburn2(end);
-fburn4 = OptACs.OptAircraft4.Mission.History.SI.Weight.Fburn(1:73) + fburn3(end);
-f = [fburn1; fburn2; fburn3; fburn4;];
-
-t1 = OptACs.OptAircraft1.Mission.History.SI.Performance.Time(1:73);
-t2 = OptACs.OptAircraft2.Mission.History.SI.Performance.Time(1:73) + t1(end) + 46*60;
-t3 = OptACs.OptAircraft3.Mission.History.SI.Performance.Time(1:73) + t2(end) + 32*60;
-t4 = OptACs.OptAircraft4.Mission.History.SI.Performance.Time(1:73)+t3(end)+ 27*60;
-t = [t1; t2; t3; t4]./60./60;
-
+History = table2timetable(mytable);
 end

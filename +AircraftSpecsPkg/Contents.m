2c698a7cdbb76e41041aacbc56a68b6cd89c79d4
prmoko <prmoko@umich.edu>
2026-04-29 14:15:21 -0400

    Updated Contents
    Added all UAVs modeled into the Contents.m file in the AircraftSpecsPkg.

--- +AircraftSpecsPkg/Contents.m
+++ +AircraftSpecsPkg/Contents.m
@@ -8,6 +8,7 @@
 % AEA                           - AircraftSpecsPkg.AEA is a function.
 % ATR42                         - AircraftSpecsPkg.ATR42 is a function.
 % CeRAS                         - AircraftSpecsPkg.CeRAS is a function.
+% DLE70MU                       - AircraftSpecsPkg.DLE70MU is a function.
 % ERJ175LR                      - AircraftSpecsPkg.ERJ175LR is a function.
 % ERJ175LR_Elec                 - AircraftSpecsPkg.ERJ175LR_Elec is a function.
 % ERJ190_E2                     - AircraftSpecsPkg.ERJ190_E2 is a function.
@@ -16,5 +17,10 @@
 % GenUAV                        - AircraftSpecsPkg.GenUAV is a function.
 % LM100J_Conventional           - AircraftSpecsPkg.LM100J_Conventional is a function.
 % LM100J_Hybrid                 - AircraftSpecsPkg.LM100J_Hybrid is a function.
+% PenguinB                      - AircraftSpecsPkg.PenguinB is a function.
+% RQ_16_THawk                   - AircraftSpecsPkg.RQ_16_THawk is a function.
 % RQ_21BlackJack                - AircraftSpecsPkg.RQ_21BlackJack is a function.
 % RQ_7AShadow                   - AircraftSpecsPkg.RQ_7AShadow is a function.
+% ScanEagle                     - AircraftSpecsPkg.ScanEagle is a function.
+% TB2                           - AircraftSpecsPkg.TB2 is a function.
+% YararaW                       - AircraftSpecsPkg.YararaW is a function.
